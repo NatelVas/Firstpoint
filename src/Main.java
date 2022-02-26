aa35c169e81fb645947218b61f6b0e7915b3823d
NatelVas <nv6579693@gmail.com>
2022-02-26 17:51:43 +0300

    task 9, 10 new

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,18 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        //task 5 6 7 8//
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        //task 9 10//
         String q ="Привет группа 132";
         String qUpper = q.toUpperCase(Locale.ROOT);
         System.out.println(qUpper);
         System.out.println(q.length());
+
 
     }
 }
